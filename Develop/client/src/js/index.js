c08b5dc5d7a6331a046fcd8f576124ba239f6389
Abi L <124858748+lailiel@users.noreply.github.com>
2023-07-25 21:28:50 -0500

    tweeks to index.js webpack.config and src.sw.js

--- Develop/client/src/js/index.js
+++ Develop/client/src/js/index.js
@@ -26,8 +26,9 @@ if (typeof editor === 'undefined') {
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
   // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
-  workboxSW.register();
+  const workboxSW = new Workbox('./src-sw.js');
+  workboxSW.register().then((reg) => console.log('service worker registered', reg))
+  .catch((err) => console.log(err));
 } else {
   console.error('Service workers are not supported in this browser.');
 }
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -23,7 +23,7 @@ butInstall.addEventListener('click', async () => {
 
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// TODO: Add a handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
 
